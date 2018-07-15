01da3d5900351ed57f183f83d9679e3fa3cfd521
Anel Drocic <anel.drocic@gmail.com>
2018-07-15 16:04:17 +0000

    initial commit

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-// mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://colt:rusty@ds055525.mongolab.com:55525/yelpcamp");
+ 
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -28,7 +28,7 @@ app.use(flash());
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret: "Once again Rusty wins cutest dog!",
+    secret: "Once again Rusty wins cutest dog!!",
     resave: false,
     saveUninitialized: false
 }));
